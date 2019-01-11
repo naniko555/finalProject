a7f95e096e5662828c9e191cc051920c2484b331
naniko555 <nanashubitidze40@yahoo.com>
2019-01-11 12:07:17 +0400

    dawdawd

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,11 @@ var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
-  return gulp.src(['style/scss/style.scss','style/scss/style-product.scss'])
+  return gulp.src(['style/scss/style-product-cart.scss','style/scss/style-product.scss','style/scss/syle-product-details.scss'])
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('style'));
 });
  
 gulp.task('watch', function () {
-  gulp.watch(['style/scss/style.scss','style/scss/style-product.scss'], gulp.parallel('sass'));
+  gulp.watch(['style/scss/style-product-cart.scss','style/scss/style-product.scss','style/scss/syle-product-details.scss'], gulp.parallel('sass'));
 }); 
