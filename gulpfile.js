b6bf27e0fe74476ab079f253b6b1f8bb54df79f2
naniko555 <nana.shubitidze@geolab.edu.ge>
2019-02-03 16:06:12 +0400

    starting boostrap

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,12 @@ var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
-  return gulp.src(['style/scss/style-product-cart.scss','style/scss/style-product.scss','style/scss/syle-product-details.scss','style/scss/style-product-main.scss'])
+  return gulp.src(['style/scss/style-product-cart.scss','style/scss/style-product-view.scss','style/scss/syle-product-details.scss','style/scss/style-product-main.scss'])
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('style'));
 });
  
 gulp.task('watch', function () {
-  gulp.watch(['style/scss/style-product-cart.scss','style/scss/style-product.scss','style/scss/syle-product-details.scss','style/scss/style-product-main.scss'], 
+  gulp.watch(['style/scss/style-product-cart.scss','style/scss/style-product-view.scss','style/scss/syle-product-details.scss','style/scss/style-product-main.scss'], 
   gulp.parallel('sass'));
 }); 
